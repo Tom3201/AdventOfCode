b085dac60a93a280b505d69f41aaecdd7c4ff412
Tom3201 <tomneuber@web.de>
2022-12-03 07:12:53 +0100

    no clue

--- 2021/Day14/code.py
+++ 2021/Day14/code.py
@@ -1,25 +1,7 @@
-from collections import Counter
+with open("data.txt") as f:
+    data, rules = f.read().split('\n\n')
 
-data, rules = open("data.txt").read().split('\n\n')
 R = {}
-for line in data.strip().split('\n'):
-    start,end = line.strip().split(' -> ')
+for line in rules.splitlines():
+    start, end = line.split(' -> ')
     R[start] = end
-
-C1 = Counter()
-for i in range(len(S)-1):
-    C1[data[i]+data[i+1]] += 1
-
-for t in range(41):
-    if t in [10,40]:
-        CF = Counter()
-        for k in C1:
-            CF[k[0]] += C1[k]
-        CF[data[-1]] += 1
-        print(max(CF.values())-min(CF.values()))
-
-    C2 = Counter()
-    for k in C1:
-        C2[k[0]+R[k]] += C1[k]
-        C2[R[k]+k[1]] += C1[k]
-    C1 = C2
