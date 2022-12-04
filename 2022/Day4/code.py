430ad6b82111ff8ef66cc7b43428b8b614b45659
Tom3201 <tomneuber@web.de>
2022-12-04 06:53:21 +0100

    add 2022/Day4: improvement

--- 2022/Day4/code.py
+++ 2022/Day4/code.py
@@ -27,10 +27,10 @@ for line in data:
     elve2min = int(elve2.split("-")[0])
     elve2max = int(elve2.split("-")[1])
     if (
-        elve2min >= elve1min and elve2max <= elve1max or
-        elve1min >= elve2min and elve1max <= elve2max or
-        elve1max >= elve2min and elve1min <= elve2min or
-        elve2max >= elve1min and elve2min <= elve1min
+        elve2min in range(elve1min, elve1max+1) or
+        elve2max in range(elve1min, elve1max+1) or
+        elve1min in range(elve2min, elve2max+1) or
+        elve1max in range(elve2min, elve2max+1)
     ):
         total += 1
 print(total)
