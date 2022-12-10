c1b0d550fe5694c803d5f58ab50fcb84db347e5d
Tom3201 <tomneuber@web.de>
2022-12-10 07:02:20 +0100

    add 2022/Day10

---
+++ 2022/Day10/code.py
@@ -0,0 +1,38 @@
+with open("data.txt") as f:
+    data = f.read().splitlines()
+
+
+def checkCycle(c, x, t):
+    if abs(x - ((c-1)%40)) < 2:
+        grid[(c-1)//40][(c-1)%40] = '#'
+    if c in [20, 60, 100, 140, 180, 220]:
+        t += c * x
+    return t
+
+
+#Part2
+global grid
+grid = [[' ' for _ in range(40)] for _ in range(6)]
+
+
+#Part1
+cycle = 0
+x = 1
+total = 0
+for line in data:
+    if line == "noop":
+        cycle += 1
+        total = checkCycle(cycle, x, total)
+    elif "add" in line:
+        a, b = line.split()
+        cycle += 1
+        total = checkCycle(cycle, x, total)
+        cycle += 1
+        total = checkCycle(cycle, x, total)
+        x += int(b)
+print(total)
+
+
+#Part2
+for a in range(6):
+    print(''.join(grid[a]))
